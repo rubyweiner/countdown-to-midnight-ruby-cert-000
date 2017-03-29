95f373159711b8929cb7a353906bf76886edbd6b
Ruby Weiner <rubyweiner796@me.com>
2017-03-29 20:54:21 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+time_left = 10
+
+def countdown (time_left)
+    while time_left > 0
+      puts "#{time_left} SECOND(S)!"
+      time_left -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(time_left)
+  while time_left > 0
+    puts "#{time_left} SECOND(S)!"
+    time_left -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
